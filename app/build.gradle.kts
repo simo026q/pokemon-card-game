9e6f0357a6cd9309640ea3c64d62d41617fa583b
Simon <simonchristensen03@gmail.com>
2024-01-31 09:42:28 +0100

    Added dependencies for REST API usage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,4 +38,11 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // https://mvnrepository.com/artifact/com.android.volley/volley
+    implementation("com.android.volley:volley:1.2.1")
+    // https://mvnrepository.com/artifact/com.google.code.gson/gson
+    implementation("com.google.code.gson:gson:2.10.1")
+    // https://mvnrepository.com/artifact/com.squareup.picasso/picasso
+    implementation("com.squareup.picasso:picasso:2.8")
 }
