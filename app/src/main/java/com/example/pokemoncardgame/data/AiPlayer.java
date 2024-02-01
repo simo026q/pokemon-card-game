539f138293ff499f97446ae3f5d82a5bb19dc423
Simon <simonchristensen03@gmail.com>
2024-02-01 10:31:58 +0100

    Improved Player object and added AiPlayer object

---
+++ app/src/main/java/com/example/pokemoncardgame/data/AiPlayer.java
@@ -0,0 +1,20 @@
+package com.example.pokemoncardgame.data;
+
+import java.util.List;
+import java.util.Random;
+
+public class AiPlayer extends Player {
+    public AiPlayer(String name, List<PokemonCardDetails> playerHand) {
+        super(name, playerHand, true);
+    }
+
+    public boolean setActiveCard() {
+        return setActiveCard(getRandomCardFromHand());
+    }
+
+    private PokemonCardDetails getRandomCardFromHand() {
+        Random random = new Random();
+        int randomIndex = random.nextInt(hand.size());
+        return hand.get(randomIndex);
+    }
+}
--- app/src/main/java/com/example/pokemoncardgame/data/Player.java
+++ app/src/main/java/com/example/pokemoncardgame/data/Player.java
@@ -1,5 +1,7 @@
 package com.example.pokemoncardgame.data;
 
+import androidx.annotation.Nullable;
+
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
@@ -7,28 +9,51 @@ import java.util.UUID;
 public class Player {
     public String id;
     public String name;
-    public boolean AI;
-
-    public AIProfile aiProfile = AIProfile.RANDOM;
+    public final boolean isAi;
 
     public List<PokemonCardDetails> hand;
-    public PokemonCardDetails activePokemon;
 
-    public Player(String name, boolean AI) {
+    @Nullable
+    public PokemonCardDetails activeCard;
+
+    public Player(String name, List<PokemonCardDetails> playerHand) {
+        this(name, playerHand, false);
+    }
+
+    protected Player(String name, List<PokemonCardDetails> playerHand, boolean isAi) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
-        this.AI = AI;
+        this.hand = playerHand;
+        this.isAi = isAi;
     }
 
-    private String playCard(PokemonCardDetails card) {
-        hand.remove(card);
-        activePokemon = card;
-        return name + " played " + card.name + "!";
+    public boolean setActiveCard(PokemonCardDetails card) {
+        if (hand.contains(card)) {
+            hand.remove(card);
+            activeCard = card;
+            return true;
+        }
+
+        return false;
     }
 
-    private String attack(PokemonCardDetails attacker, PokemonCardDetails defender, Attack attack) {
+    public void attackWithCard(PokemonCardDetails attackerCard) {
+        if (activeCard != null) {
+            activeCard.hp -= attackerCard.getAttackDamage();
+
+            if (!activeCard.isAlive()) {
+                activeCard = null;
+            }
+        }
+    }
+
+    public boolean hasLost() {
+        return hand.size() == 0 && activeCard != null && !activeCard.isAlive();
+    }
+
+    /*private String attack(PokemonCardDetails attacker, PokemonCardDetails defender, Attack attack) {
         if (attacker.hp <= 0) {
-            activePokemon = null;
+            activeCard = null;
             return attacker.name + " has fainted and cannot attack!";
         }
         if (attack.damage > 0) {
@@ -38,56 +63,5 @@ public class Player {
             return attacker.name + " attacked " + defender.name + " for " + attack.damage + " damage and knocked it out!";
         }
         return attacker.name + " attacked " + defender.name + " for " + attack.damage + " damage!";
-    }
-
-    private boolean activeDead() {
-        if (activePokemon != null && activePokemon.hp <= 0) {
-            activePokemon = null;
-            return true;
-        }
-        return false;
-    }
-
-    private boolean hasLost() {
-        return hand.size() == 0 && activePokemon.hp <= 0;
-    }
-
-    public void playAICard(Player opponent) {
-        Random rand = new Random();
-        /*if (activeDead()) {
-            if (AIProfile.RANDOM.equals(aiProfile)) {
-                 playCard(hand.get(rand.nextInt(hand.size() - 1)));
-            }
-            else {
-                for (PokemonCardDetails card : hand) {
-                    if (AIProfile.PASSIVE.equals(aiProfile)) {
-                        if (card.hp > 0 && card.hp < opponent.activePokemon.hp) {
-                            playCard(card);
-                            break;
-                        }
-                        else {
-                            playCard(card);
-                            break;
-                        }
-                    } else if (AIProfile.AGGRESSIVE.equals(aiProfile)) {
-                        if (card.hp > 0) {
-                            for (Attack attack : card.attacks) {
-                                if (attack.damage > 0 && attack.damage >= opponent.activePokemon.hp) {
-                                    playCard(card);
-                                    break;
-                                }
-                                else {
-                                    playCard(card);
-                                    break;
-                                }
-                            }
-                            if (activePokemon != null) {
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }*/
-    }
+    }*/
 }
