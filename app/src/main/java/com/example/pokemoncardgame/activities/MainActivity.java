4c08e30313a0f9861c844058013206ca2aa2df66
lukasdreier040403 <lukasryborgdreier@gmail.com>
2024-02-02 08:21:38 +0100

    Added ui to main activity

--- app/src/main/java/com/example/pokemoncardgame/activities/MainActivity.java
+++ app/src/main/java/com/example/pokemoncardgame/activities/MainActivity.java
@@ -1,40 +1,41 @@
 package com.example.pokemoncardgame.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
-
 import com.example.pokemoncardgame.R;
-import com.example.pokemoncardgame.data.network.PokemonCardService;
 
 public class MainActivity extends AppCompatActivity {
-
-    private PokemonCardService pokemonCardService;
-
-    private TextView debugTextView;
+    private EditText numCardsInput;
+    private Button startGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        debugTextView = findViewById(R.id.tv_debug);
+        numCardsInput = findViewById(R.id.numCardsInput);
+        startGameButton = findViewById(R.id.startGameButton);
 
-        pokemonCardService = new PokemonCardService(this);
-        pokemonCardService.getRandomCards(10, cards -> {
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < cards.size(); i++) {
-                sb.append(cards.get(i).name);
-                sb.append("\n");
+        startGameButton.setOnClickListener(view -> {
+            String numCardsStr = numCardsInput.getText().toString();
+            if (!numCardsStr.isEmpty()) {
+                int numCards = Integer.parseInt(numCardsStr);
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                intent.putExtra("numberOfCards", numCards);
+                startActivity(intent);
+                startGameButton.setEnabled(false);
             }
-
-            String debugText = sb.toString();
-
-            this.runOnUiThread(() -> {
-                debugTextView.setText(debugText);
-            });
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startGameButton.setEnabled(true); // Re-enable the button when the activity resumes
     }
 }
